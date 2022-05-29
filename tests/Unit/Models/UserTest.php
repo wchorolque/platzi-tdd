954143b69a757e8140e7e1b73cf2fa9431838df1
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-29 00:27:33 -0400

    Completado 4/23: Relaciones:multiples
    - php artisan --filter test_has_many_repositories

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -4,7 +4,7 @@ namespace Tests\Unit\Models;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class UserTest extends TestCase
 {
