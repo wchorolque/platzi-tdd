0eabd236f86a89ac2fdd2d7e9d333d77489566a1
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-29 02:04:05 -0400

    Creado funcion a probar y test falla
    - $this->get('repositories')->assertRedirect('login');

--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -8,15 +8,8 @@ use Tests\TestCase;
 
 class RepositoryControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function test_guest()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->get('repositories')->assertRedirect('login');
     }
 }
