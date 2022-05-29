14067140ad8d9ef8acee5494c1a2d103417c2a8a
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-29 02:18:20 -0400

    Completado 6/23: Proteger rutas
    - Agregado test para todas las rutas posibles del controlador
      RepositoryController.

--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -10,6 +10,19 @@ class RepositoryControllerTest extends TestCase
 {
     public function test_guest()
     {
+        // index
         $this->get('repositories')->assertRedirect('login');
+        // show
+        $this->get('repositories/1')->assertRedirect('login');
+        // edit
+        $this->get('repositories/1/edit')->assertRedirect('login');
+        // update
+        $this->put('repositories/1')->assertRedirect('login');
+        // destroy
+        $this->delete('repositories/1')->assertRedirect('login');
+        // create
+        $this->get('repositories/create')->assertRedirect('login');
+        // guardar
+        $this->post('repositories', [])->assertRedirect('login');
     }
 }
