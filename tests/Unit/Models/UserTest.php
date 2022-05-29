40447acd7614d2f48173810d48d577d4bcc51923
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-29 00:21:02 -0400

    Modificado UserTest.php

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
@@ -11,8 +13,11 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_has_many_repositories()
     {
         $this->assertTrue(true);
+        $user = new User();
+
+        $this->assertInstanceOf(Collection::class, $user->repositories);
     }
 }
