060d859ba54abb6be7c783a420f5a761190fc6dd
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-29 02:08:31 -0400

    Agregado ruta en web.php
    - Route::resource('repositories',
      App\Http\Controllers\RepositoryController::class)->middleware('auth');

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('repositories', App\Http\Controllers\RepositoryController::class)
+    ->middleware('auth');
+
