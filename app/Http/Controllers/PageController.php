7db0ddbe93c29475999b378035a9368ef701b653
williams chorolque <wchorolque@gmail.com>
2022-06-05 23:42:41 -0400

    Agregado relacion user a la consulta

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
     public function home()
     {
         return view('welcome', [
-            'repositories' => Repository::get()
+            'repositories' => Repository::with('user')->get()
         ]);
     }
 }
