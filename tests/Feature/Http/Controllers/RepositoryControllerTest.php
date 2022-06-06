7ab98e7206389e584ac2239b1798804150de09e4
williams chorolque <wchorolque@gmail.com>
2022-06-05 20:46:09 -0400

    Agregado test test_create
    - Para visualizar el formulario de creacion

--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -198,4 +198,14 @@ class RepositoryControllerTest extends TestCase
             ->get("repositories/$repository->id")
             ->assertStatus(403);
     }
+
+    public function test_create()
+    {
+        $user = User::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->get('repositories/create')
+            ->assertStatus(200);
+    }
 }
