a1ce5756697301f7e76fba9283c1cba1c972e0a1
williams chorolque <wchorolque@gmail.com>
2022-06-05 22:01:11 -0400

    Agregado metodo a ruta home en PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Repository;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('welcome', [
+            'repositories' => Repository::latest()->get()
+        ]);
+    }
 }
